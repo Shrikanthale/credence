405f5633f58d34d91906b0331a66b3a621bb9a8e
shrikant <skanthale9@gmail.com>
2021-10-04 11:14:57 +0530

    credence project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './Tvfseries.js';
+import './style.css';
+import './Card.js'
+function App() {
+  return (
+    <div>
+       {/* <Tvfseries/> */}
+    </div>
+  );
+}
+
+export default App;
